4eb383097c790a9c0e3e70ec4e9af5c5664368c2
mundinbass <firecrack78@gmail.com>
2022-10-03 21:47:57 -0400

    Added logging

--- Pine/src/Pine.h
+++ Pine/src/Pine.h
@@ -3,6 +3,7 @@
 // For use by Pine applications
 
 #include "Pine/Application.h"
+#include "Pine/Log.h"
 
 // --- Entry Point ----------
 #include "Pine/EntryPoint.h"
--- Pine/src/Pine/EntryPoint.h
+++ Pine/src/Pine/EntryPoint.h
@@ -7,6 +7,11 @@ extern Pine::Application* Pine::CreateApplication();
 
 int main(int argc, char** argv)
 {
+	Pine::Log::Init();
+	PN_CORE_WARN("Initialized Log!");
+	int a = 5;
+	PN_INFO("Hello! Var={0}", a);
+
 	auto app = Pine::CreateApplication();
 	app->Run();
 	delete app;
