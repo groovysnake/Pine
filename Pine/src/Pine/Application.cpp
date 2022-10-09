c584b952c7b6b7f1e8525e7dfc9c3a109e0d9c15
mundinbass <firecrack78@gmail.com>
2022-10-08 22:31:38 -0400

    Added Event system

--- Pine/src/Pine/Application.cpp
+++ Pine/src/Pine/Application.cpp
@@ -1,5 +1,8 @@
 #include "Application.h"
 
+#include "Pine/Events/ApplicationEvent.h"
+#include "Pine/Log.h"
+
 namespace Pine
 {
 	Application::Application()
@@ -12,6 +15,9 @@ namespace Pine
 	}
 	void Application::Run()
 	{
+		WindowResizeEvent e(1280, 720);
+		PN_TRACE(e);
+
 		while (true);
 	}
 
